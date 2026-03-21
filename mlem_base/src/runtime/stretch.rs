685c1fbe9603a7c826c27d3ddab0bfb7a8afdf0d
Puk Bruinsma <Spookezatemyneighbours@gmail.com>
2026-03-21 11:51:17 +0100

    WIP.

--- mlem_base/src/runtime/stretch.rs
+++ mlem_base/src/runtime/stretch.rs
@@ -47,6 +47,7 @@ impl Stretch {
 
     pub fn reset(&mut self) {
         self.buffer.clear();
+        self.buffer.push_back(0.0);
         self.buffer_pos = 0;
         self.delta = 0;
     }
@@ -55,18 +56,11 @@ impl Stretch {
         let input = input.as_ref();
         let output = output.as_mut();
 
-        if self.buffer.is_empty() {
-            if !self.try_update_curr(input, output, 0) {
-                self.buffer.push_back(0.0);
-            }
-        }
-
         let mut index = 0;
-        
         while index < output.len() {
             if self.buffer_pos >= self.buffer.len() && self.delta < 0 {
                 let from = (index as f32 / output.len() as f32 * input.len() as f32) as usize;
-                self.try_update_curr(input, output, from);
+                // TODO update
             }
 
             output[index] = self.buffer[self.buffer_pos % self.buffer.len()];
@@ -98,25 +92,6 @@ impl Stretch {
             output[i] = input[in_i];
         }
         */
-    }
-
-    fn try_update_curr(&mut self, input: &[f32], output: &[f32], from: usize) -> bool {
-        let target = usize::min(from + self.window_len(), input.len() - 1);
-        let next_zero = utils::next_zero_crossing(input, target);
-        
-        if next_zero == target { return false; }
-
-        self.buffer.clear();
-        self.buffer_pos = 0;
-
-        for i in from..next_zero {
-            self.buffer.push_back(input[i]);
-        }
-
-        let stretch_factor = output.len() as f32 / input.len() as f32;
-        self.delta += f32::floor(self.buffer.len() as f32 * stretch_factor) as isize;
-        
-        return false;
     }
 
     fn window_len(&self) -> usize {
